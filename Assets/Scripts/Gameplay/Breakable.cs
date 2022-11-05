43453607a016a2324ccc5a4c58187c762bbff1e3
Bill Spooner <billjspooner@gmail.com>
2022-11-05 15:50:00 +0000

    feat: add percent counter - tweak level

--- Assets/Scripts/Gameplay/Breakable.cs
+++ Assets/Scripts/Gameplay/Breakable.cs
@@ -1,46 +1,55 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
-public class Breakable : MonoBehaviour
+namespace Gameplay
 {
-    [FormerlySerializedAs("Health")] [Header("Config")]
-    public int health = 1;
-    [Header("References")]
-    public GameObject main;
-    public GameObject insides;
-
-    private SpriteRenderer _sprite;
-    private Collider2D _collider;
-    private List<Rigidbody2D> _innerRigidbodies;
-    private int _currentHealth;
-
-    private void Awake()
+    public class Breakable : MonoBehaviour
     {
-        _sprite = GetComponentInChildren<SpriteRenderer>();
-        _currentHealth = health;
-        _innerRigidbodies = insides.GetComponentsInChildren<Rigidbody2D>().ToList();
+        public Action Broken;
 
-        insides.SetActive(false);
-    }
+        [FormerlySerializedAs("Health")] [Header("Config")]
+        public int health = 1;
+        [Header("References")]
+        public GameObject main;
+        public GameObject insides;
 
-    public void Break(Vector2 force)
-    {
-        _currentHealth--;
+        private SpriteRenderer _sprite;
+        private Collider2D _collider;
+        private List<Rigidbody2D> _innerRigidbodies;
+        private int _currentHealth;
 
-        if (_sprite != null) _sprite.color = Color.Lerp(Color.black, Color.white, (float)_currentHealth / health);
-
-        if(_currentHealth > 0) return;
-
-        main.SetActive(false);
-        insides.transform.parent = transform;
-        insides.SetActive(true);
-
-        foreach (var innerRigidbody in _innerRigidbodies)
+        private void Awake()
         {
-            innerRigidbody.AddForce(force, ForceMode2D.Impulse);
-            innerRigidbody.AddTorque(Random.Range(-1f, 1f) * force.magnitude / 5f, ForceMode2D.Impulse);
+            _sprite = GetComponentInChildren<SpriteRenderer>();
+            _currentHealth = health;
+            _innerRigidbodies = insides.GetComponentsInChildren<Rigidbody2D>().ToList();
+
+            insides.SetActive(false);
+        }
+
+        public void Break(Vector2 force)
+        {
+            _currentHealth--;
+
+            if (_sprite != null) _sprite.color = Color.Lerp(Color.black, Color.white, (float)_currentHealth / health);
+
+            if(_currentHealth > 0) return;
+
+            main.SetActive(false);
+            insides.transform.parent = transform;
+            insides.SetActive(true);
+
+            foreach (var innerRigidbody in _innerRigidbodies)
+            {
+                innerRigidbody.AddForce(force, ForceMode2D.Impulse);
+                innerRigidbody.AddTorque(Random.Range(-1f, 1f) * force.magnitude / 5f, ForceMode2D.Impulse);
+            }
+
+            Broken?.Invoke();
         }
     }
 }
---
+++ Assets/Scripts/Gameplay/BreakableTracker.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class BreakableTracker : MonoBehaviour
+    {
+        public TextMeshProUGUI percentText;
+
+        private List<Breakable> _breakables;
+        private int _currentBroken;
+
+        private void Awake()
+        {
+            _breakables = GetComponentsInChildren<Breakable>().ToList();
+
+            _breakables.ForEach(b => b.Broken += OnBreak);
+        }
+
+        private void OnBreak()
+        {
+            _currentBroken++;
+        }
+
+        private void Update()
+        {
+            if (percentText)
+            {
+                percentText.text = $"{decimal.Round((decimal)((float)_currentBroken / _breakables.Count * 100))}%";
+            }
+        }
+    }
+}
